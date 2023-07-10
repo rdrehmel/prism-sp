e375c5c90caa5342cb33b1d38ad67a1aef00afb6
rdrehmel <robert@zoot.drehmel.com>
2023-07-11 00:25:37 +0200

    Delete sp-update.sh

--- firmware/sp-update.sh
+++
@@ -1,3 +0,0 @@
-DEST=../../../test/petalinux-zcu102-prism-sp-demo/project-spec/meta-user/recipes-apps/prism-sp-firmware/files/
-cp prism-sp-duo-rx-firmware.elf ${DEST}/prism-sp-duo-rx-firmware.elf
-cp prism-sp-duo-tx-firmware.elf ${DEST}/prism-sp-duo-tx-firmware.elf
